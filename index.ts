38b4e5a6e4f83c02c3c359ab9cc801f8845b0e56
shir <shir8806@gmail.com>
2022-12-21 16:56:00 +0200

    my solution

--- index.ts
+++ index.ts
@@ -1,38 +1,47 @@
+//* EventEmitter class to track events:
+//* Define events interface:
+interface Events {
+    [key: string]: Function[]; // using an array because there could be more than one subscriber for each event.
+  }
+
 class EventEmitter {
-    events : {key: string, function:[]} | {} = {}
-
-    
-    
-    //* Subscription:
-    static subscribe () {
-
+    public events: Events;
+    constructor(events?: Events) {
+      this.events = events || {};
     }
-
-    //* Subscription:
-    static unsubscribe () {
-
+  
+    //* Subscription + Unsubscription:
+    public subscribe (eventName: string, cb: Function) {
+        (this.events[eventName] || (this.events[eventName] = [])).push(cb);
+        return {
+            unsubscribe: () =>
+              this.events[eventName] && this.events[eventName].splice(this.events[eventName].indexOf(cb) >>> 0, 1) // using bitwise operator to get a real number every time we call splice()
+          };
     }
-
-
+    
     //* Emitting:
-    static emit () {
-
-    }
-
-
-
+    public emit(eventName: string, ...args: any[]): void {
+        (this.events[eventName] || []).forEach(fn => fn(...args)); // iterate through the array of callbacks that have been stored. 
+      }
 }
 
 
 
 function logKeydown (event) {
-    console.log('keyboard pressed: ', event.key);
+    console.log('keyboard pressed: ' + event.key);
 }
 function alertKeydown (event) {
     alert('keyboard pressed: ' + event.key);
 }
 
-const logKeydownSubscription = EventEmitter.subscribe('keydown', logKeydown);
-const alertKeydownSubscription = EventEmitter.subscribe('keydown', alertKeydown);
+const logKeydownSubscription =  new EventEmitter;
+logKeydownSubscription.subscribe('keydown', logKeydown);
 
+const alertKeydownSubscription = new EventEmitter;
+alertKeydownSubscription.subscribe('keydown', alertKeydown);
+
+EventEmitter.emit('keydown', {key: 'Enter'});
+
+alertKeydownSubscription.unsubcribe();
+logKeydownSubscription.unsubscribe();
 
